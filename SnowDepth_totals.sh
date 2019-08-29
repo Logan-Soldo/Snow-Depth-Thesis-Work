8ac6f7350fb235b0beaed309ee9ebb70e2ac5910
ScruffyBear013 <54323300+ScruffyBear013@users.noreply.github.com>
2019-08-29 13:23:06 -0400

    Add files via upload

--- SnowDepth_totals.sh
+++ SnowDepth_totals.sh
@@ -11,20 +11,20 @@
 #5.  Latitude of 89x89 cell (decimal degrees)
 #6.  Maximum elevation of stations reporting snowfall in cell (m)
 #7.  Minimum elevation of stations reporting snowfall in cell (m)
-#8.  Mean snow depth of stations reporting snowfall in cell (cm)
-#9.  Median snow depth of stations reporting snowfall in cell (cm)
+#8.  Mean snow depth of stations reporting snowfall in cell (mm)
+#9.  Median snow depth of stations reporting snowfall in cell (mm)
 #10. Number of stations reporting in file
-#11. Number of stations reporting > 10cm snowfall in cell
-#12. Number of stations reporting > 25cm snowfall in cell
-#13. Number of stations reporting > 50cm snowfall in cell
-#14. Number of stations reporting > 100cm snowfall in cell
+#11. Number of stations reporting > 10mm snowfall in cell
+#12. Number of stations reporting > 25mm snowfall in cell
+#13. Number of stations reporting > 50mm snowfall in cell
+#14. Number of stations reporting > 100mm snowfall in cell
 #15. NOAA cell area
 #16. NOAA snow cover flag (1=snow covered) (not included)
  
 
  pgm=SnowDepth_totals.exe
 
- set -x
+ #set -x
 
 ##### Home Directory ######
 #datadir=/mnt/e/School/Thesis/SnowDepth_Data/Outputs
@@ -32,22 +32,22 @@
 
 ##### School Directory ######
 datadir=/mnt/u/Research/SnowDepth_Data/Outputs
+datadir2=/mnt/u/Research/Snowfall_Data/Outputs
 outputdir=/mnt/u/Research/SnowDepth_Data/Outputs
 
 
 start_time=$(date +%s)
-#echo $start_time
+echo $start_time
 
  
-#list="2741 2740 2739 2738 2737 2641 2640 2639 2638 2541 2540 2539 2538 2441 2440 2439 2438 2341 2340 2339  2241 2240 2141"
-list="2340 2439 2440 2540 2541 2641 2739 2740 2741 2737 2638 2539 2640 2341 2538"  # missing greater than 25% of data.
+Blacklist="2741 2538 2441 2440 2439 2438 2341 2340 2339 2241 2240 2141" 
 
 for i in {12..27}
 do
 	for j in {33..41}
 		
 		do 
-		 if [[ $list =~ $i$j ]]; then
+		 if [[ $Blacklist =~ $i$j ]]; then
 			 continue 							# If the number is in the list of cells not being used then it will skip this cell and proceed to the next cell.
 		 fi
 
@@ -63,10 +63,13 @@ do
 
     
  for filein in `ls -d $datadir/$i$j/${i}_${j}_SnowDepth.txt`
+ do
+ for filein2 in `ls -d $datadir2/$i$j/Snowfall/${i}${j}_SnwfCountDate.txt`
    
 
    do
       filename=`basename $filein|sed 's/\.txt//'`
+	  filename2=`basename $filein2|sed 's/\.txt//'`
 	fileout=$outputdir/$i$j/SnowDepth/
 	fileout2=$outputdir/All/PercentMiss/
 	fileout3=$outputdir/$i$j/SnowDepth/Junk/
@@ -78,14 +81,16 @@ do
       cat <<EOF > cdh.nml
 &cdh_nml
 inputfile="${filein}",
+inputfile2="${filein2}",
 outputfile="${fileout}${i}${j}_SplitDate.txt",
 outputfile2="${fileout}${i}${j}_CountDate.txt",
 outputfile3="${fileout}${i}${j}_MeanSnowDepth.txt",
 outputfile4="${fileout}${i}${j}_SDQuality.txt",
 outputfile5="${fileout2}${i}${j}_PercentMiss.txt",
 outputfile6="${fileout}${i}${j}_DaySnowDepth.txt",
-outputfile7="${fileout}${i}${j}_SeasonalSnowDepth.txt",
-outputfile8="${fileout3}${i}${j}_Junk.txt",
+outputfile7="${fileout}${i}${j}_MonthlyAverage.txt",
+outputfile8="${fileout}${i}${j}_SeasonalSnowDepth.txt",
+outputfile9="${fileout3}${i}${j}_Junk.txt",
 /
 EOF
 
@@ -102,6 +107,7 @@ EOF
       done
 	 done
 	done
+	done
 	 
 
 	 # for ij in 2741 2740 2739 2738 2737 2641 2640 2639 2638 2541 2540 2539 2538 2441 2440 2439 2438 2341 2340 2339  2241 2240 2141
@@ -115,6 +121,5 @@ EOF
 	
 
 #rm -f data1.bin
-end_time=$(date +%s)
-runtime=$((($(date +%s)-$start_time)/60))
-echo "END OF SCRIPT at $(date) in $runtime minutes"
+
+echo "END OF SCRIPT at $(date)"
